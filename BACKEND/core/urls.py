bd50037550d1deb78e154f6fc6c4e4017977617f
Allaudin Ansari <allu456654ansari@gmail.com>
2025-04-01 09:29:09 +0530

    updated

--- BACKEND/core/urls.py
+++ BACKEND/core/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path("", home, name="home"), 
     path("generate_sql/", GenerateSQLQueryView.as_view(), name="generate_sql"),
     path("execute_sql/", ExecuteSQLQueryView.as_view(), name="execute_sql"),
-    path("query_history/", QueryHistoryView.as_view(), name="query_history"),  # ✅ Add this line
+    path("query_history/", QueryHistoryView.as_view(), name="query_history"),  
 
 ]
